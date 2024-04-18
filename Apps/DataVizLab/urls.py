596c88c10a8b81e3d1683c8d4424c4ccab073737
Mañanas <sgranjot@gmail.com>
2024-04-18 09:42:21 +0200

    idex page routing changed

--- Apps/DataVizLab/urls.py
+++ Apps/DataVizLab/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required
 
 from . import views
+from Apps.account.views import dashboard
 
 app_name = 'DataVizLab'
 urlpatterns = [
-    #path('', views.upload_excel, name='upload_excel'),            # ex: /Apps/DataVizLab/
+    path('', login_required(dashboard), name='index'),            # ex: /Apps/DataVizLab/
     path('create_ExcelFile/', login_required(views.CreateExcelFileView.as_view()), name='create_ExcelFile'),
     #path('create_table/', views.create_table, name='create_table'),
     path('list_ExcelFiles/', login_required(views.ListExcelFileView.as_view()), name='list_ExcelFiles'),
